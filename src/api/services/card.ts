ae1d169e8cd09a43c9c34d6763b48a372d169243
bamidele2161 <akinyemibamidele2@gmail.com>
2025-08-18 08:55:28 -0700

    Set up hooks for other card APIs

--- src/api/services/card.ts
+++ src/api/services/card.ts
@@ -3,6 +3,15 @@ import type { CardResponse } from "@/types/card";
 
 export const getCardRequest = async (): Promise<CardResponse> => {
   const res = await axiosInstance.get<CardResponse>("/cards/requests");
-  console.log(res);
+  return res.data;
+};
+export const getCardSummary = async (): Promise<any> => {
+  const res = await axiosInstance.get<CardResponse>("/cards/requests/summary");
+  return res.data;
+};
+export const getCardChartData = async (): Promise<any> => {
+  const res = await axiosInstance.get<CardResponse>(
+    "/cards/requests/chart-data"
+  );
   return res.data;
 };
--- src/hooks/useCard.ts
+++ src/hooks/useCard.ts
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCardRequest } from "@/api/services/card";
+import {
+  getCardChartData,
+  getCardRequest,
+  getCardSummary,
+} from "@/api/services/card";
 
 export function useGetCards() {
   const { data, error, isPending } = useQuery({
@@ -13,6 +17,44 @@ export function useGetCards() {
     console.log(error);
   }
   console.log(data, "wha tis going on");
+  return {
+    data: data,
+    error,
+    isPending,
+  };
+}
+
+export function useGetCardSummary() {
+  const { data, error, isPending } = useQuery({
+    queryKey: ["cardSummary"],
+    queryFn: () => getCardSummary(),
+    retry: false,
+  });
+
+  if (error) {
+    // toast.error(error.message);
+    console.log(error);
+  }
+
+  return {
+    data: data,
+    error,
+    isPending,
+  };
+}
+
+export function useGetChartData() {
+  const { data, error, isPending } = useQuery({
+    queryKey: ["cardChartDataList"],
+    queryFn: () => getCardChartData(),
+    retry: false,
+  });
+
+  if (error) {
+    // toast.error(error.message);
+    console.log(error);
+  }
+
   return {
     data: data,
     error,
